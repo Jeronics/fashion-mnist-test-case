6a8eaec9d9eed7fb34d913e2b60c6d3234b5e107
Jeroni <jeronicarandell@gmail.com>
2020-06-01 16:27:20 +0200

    Add class list names in config.

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,5 @@
-CLASS_LIST = ['FULL_VISIBILITY', 'PARTIAL_VISIBILITY', 'NO_VISIBILITY']
+
+LIST_CLASS = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
 
 # Global variables related to the dataset and the image locations.
 DATA_DIR = 'data/'
@@ -8,4 +9,3 @@ RESULTS_DIR = 'results/'
 # These are the mean and standard deviation of the pixel value of the whole dataset.
 MEAN_PIXEL = 0.28604084
 STD_PIXEL = 0.35302418
-
