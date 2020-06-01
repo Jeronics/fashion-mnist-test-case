0a1a9f32b3e24e4548dc407df132056487c818f5
Jeroni <jeronicarandell@gmail.com>
2020-06-02 01:41:04 +0200

    Add convoluted sliding window.

--- models/networks.py
+++ models/networks.py
@@ -21,6 +21,38 @@ class CNN(nn.Module):
         return x
 
 
+class ProductionCNN(CNN):
+    def __init__(self, state_dict=None):
+        super(ProductionCNN, self).__init__()
+        # 1 gray channel, 5 kernels, 5x5 kenel size
+        if state_dict is not None:
+            self.load_state_dict(state_dict)
+
+        # Convert the original fc layer to a convolutional layer.
+        inch = 16
+        out = 80
+        kernel = 5
+        self.conv1.stride=1
+
+        self.last_conv1 = nn.Conv2d(in_channels=inch, out_channels=out, kernel_size=kernel)
+        self.last_conv1.weight.data.copy_(self.fc1.weight.data.view(out, inch, kernel, kernel))
+        self.last_conv1.bias.data.copy_(self.fc1.bias.data)
+
+        print("in", self.fc2.in_features, "out", self.fc2.out_features)
+        print(*self.fc2.weight.data.shape)
+        self.last_conv2 = nn.Conv2d(in_channels=self.fc2.in_features, out_channels=10, kernel_size=1)
+        self.last_conv2.weight.data.copy_(self.fc2.weight.data.view(*self.fc2.weight.data.shape, 1, 1))
+        self.last_conv2.bias.data.copy_(self.fc2.bias.data)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x
+        x = F.relu(self.last_conv1(x))
+        x = self.last_conv2(x)
+        return x
+
+
 class CNN2(nn.Module):
     def __init__(self):
         super(CNN2, self).__init__()
