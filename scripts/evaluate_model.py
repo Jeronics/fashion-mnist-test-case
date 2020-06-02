8d57c014549186e67744af4fc1d6f8668a3f7520
Jeroni <jeronicarandell@gmail.com>
2020-06-02 13:36:20 +0200

    Fix non normalized data.

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -4,12 +4,12 @@ from os import path
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
-from utils.config import ARTIFACTS_DIR, DATA_DIR
+from utils.config import ARTIFACTS_DIR, DATA_DIR, MEAN_PIXEL, STD_PIXEL
 from utils.data_loaders import CustomFashionMNIST
 from utils.model_evaluation import ModelEvaluator
 
 if __name__ == '__main__':
-    model_name = 'cnn_1'
+    model_name = 'cnn_3'
 
     # loading
     with open(path.join(ARTIFACTS_DIR, model_name + '.pkl'), 'rb') as f:
@@ -17,16 +17,15 @@ if __name__ == '__main__':
 
     # Define the transformations applied to the training data
     transform = transforms.Compose(
-        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+        [transforms.ToTensor(), transforms.Normalize((MEAN_PIXEL,), (STD_PIXEL,))]
     )
-    testset = CustomFashionMNIST(DATA_DIR, download=True, train=False, transform=transform)
 
-    train_loader = DataLoader(testset, batch_size=128, shuffle=True, num_workers=2)
+    testset = CustomFashionMNIST(DATA_DIR, download=True, train=False, transform=transform)
 
     modelEvaluator = ModelEvaluator(cvGridSearch.best_model)
     modelEvaluator.fit(testset)
     print(modelEvaluator.get_accuracy())
     print(modelEvaluator.get_confusion_matrix())
     modelEvaluator.save_results(model_name)
-    print(cvGridSearch.best_params_)
-    print(cvGridSearch.best_model.module_)
+    # print(cvGridSearch.best_params_)
+    # print(cvGridSearch.best_model.module_)
